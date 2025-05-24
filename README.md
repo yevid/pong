959d24c5b45ad0347163626701b0d7b5fdf94555
David Klausen <david.klausen@icloud.com>
2025-05-24 11:31:19 +0200

    Add background music

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # pong
 The simple video game Pong written in Pygame.
+Music used: "Moon Theme" from Ducktales on the NES
--- game.py
+++ game.py
@@ -1,6 +1,12 @@
 import pygame
 pygame.init()
 
+font = pygame.font.SysFont(None, 48)
+
+# Background music
+pygame.mixer.music.load('background.mp3')
+pygame.mixer.music.play(-1)  # Loop the music indefinitely
+
 # Window size
 WIDTH, HEIGHT = 900, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -9,13 +15,17 @@ pygame.display.set_caption('Pong')
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 # Paddle and ball
 PADDLE_W, PADDLE_H = 15, 100
 BALL_SIZE = 20
+
+# Scoreboard
+left_score = 0
+right_score = 0
 
 left_paddle = pygame.Rect(30, HEIGHT//2 - PADDLE_H//2, PADDLE_W, PADDLE_H)
 right_paddle = pygame.Rect(WIDTH-45, HEIGHT//2 - PADDLE_H//2, PADDLE_W, PADDLE_H)
@@ -50,10 +60,10 @@ while running:
     # Ball collision with top/bottom
     if ball.top <= 0:
         ball.top = 0
-        ball_speed_y *= -1
+        ball_speed_y = ball_speed_y * -1
     if ball.bottom >= HEIGHT:
         ball.bottom = HEIGHT
-        ball_speed_y *= -1
+        ball_speed_y = ball_speed_y * -1 
 
     # Ball collision with paddles
     if ball.colliderect(left_paddle) or ball.colliderect(right_paddle):
@@ -62,12 +72,21 @@ while running:
 
 
     # Ball reset if out of bounds
-    if ball.left <= 0 or ball.right >= WIDTH:
+    if ball.left <= 0:
         ball.center = (WIDTH//2, HEIGHT//2)
         ball_speed_x, ball_speed_y = 3, 3
+        right_score = right_score + 1
+    if ball.right >= WIDTH: 
+        ball.center = (WIDTH//2, HEIGHT//2)
+        ball_speed_x, ball_speed_y = -3, -3
+        left_score = left_score + 1
 
     # Drawing
     screen.fill(BLACK)
+    left_text = font.render(str(left_score), True, WHITE)
+    right_text = font.render(str(right_score), True, WHITE)
+    screen.blit(left_text, (WIDTH//4, 20))
+    screen.blit(right_text, (WIDTH*3//4, 20))
     pygame.draw.rect(screen, RED, left_paddle)
     pygame.draw.rect(screen, GREEN, right_paddle)
     pygame.draw.ellipse(screen, BLUE, ball)
